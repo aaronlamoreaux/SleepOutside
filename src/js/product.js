fcab0d63adb7c604ca8f70e50e51453282ea2d28
Aaron Lamoreaux <lam22013@byui.edu>
2024-04-26 13:37:41 -0700

    W01 Individual Activity: Add Item to Cart Broken

--- src/js/product.js
+++ src/js/product.js
@@ -2,6 +2,7 @@ import { setLocalStorage } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 
 const dataSource = new ProductData("tents");
+let products = [];
 
 function addProductToCart(product) {
   setLocalStorage("so-cart", product);
@@ -9,7 +10,8 @@ function addProductToCart(product) {
 // add to cart button event handler
 async function addToCartHandler(e) {
   const product = await dataSource.findProductById(e.target.dataset.id);
-  addProductToCart(product);
+  products.push(product);
+  addProductToCart(products);
 }
 
 // add listener to Add to Cart button
